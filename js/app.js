265ef0ff5ba4d7950d2b4dff266b1a2443b0f071
MotasemZiad <motasemabunima@gmail.com>
2023-04-26 16:54:08 +0200

    finish the status counting

---
+++ js/app.js
@@ -0,0 +1,29 @@
+// statistics numbers section
+let statisticsNumbers = document.querySelectorAll(".statistics .box .number");
+let statisticsSection = document.querySelector(".statistics");
+let started = false;
+
+window.onscroll = () => {
+  if (window.scrollY >= statisticsSection.offsetTop - 200) {
+    if (!started)
+      statisticsNumbers.forEach((element) => startCounting(element));
+    started = true;
+  }
+};
+
+function startCounting(element) {
+  let goal = element.dataset.goal;
+  let count = setInterval(() => {
+    if (isNaN(element.textContent)) {
+      let parsedInt = parseInt(element.textContent);
+      parsedInt++;
+      element.textContent = parsedInt + "K";
+      if (element.textContent === goal + "K") clearInterval(count);
+    } else {
+      element.textContent++;
+      if (element.textContent === goal) clearInterval(count);
+    }
+  }, 2000 / goal);
+}
+
+// skills progress section
